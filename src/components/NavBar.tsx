061d19a148350316e19b694a27277cd551267e8b
Sai <sai@SAIs-MacBook-Pro.local>
2023-11-24 22:04:38 +0530

    adding responsive styles

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,15 +6,18 @@ import {
 	Instagram,
 	Facebook,
 	UserRound,
-	ShoppingBag,
+	AlignJustify,
 	Search,
 } from "lucide-react";
 import CartIcon from "./CartIcon";
 
 function NavBar() {
 	return (
-		<nav className="flex justify-between p-2 secondary-color text-white">
-			<div className="flex space-x-7 items-center pl-10">
+		<nav className="flex justify-between p-2 secondary-color text-white h-16 md:h-[88px]">
+			<div className="md:hidden flex items-center">
+				<AlignJustify />
+			</div>
+			<div className="hidden md:flex space-x-7 items-center pl-10">
 				<Menu name="brands" />
 				<Menu name="shop" />
 				<div className="underline-yellow hover:underline-yellow">premium</div>
@@ -22,7 +25,7 @@ function NavBar() {
 					gift cards
 				</div>
 			</div>
-			<div>
+			<div className="hidden md:block">
 				<Image
 					src="/gweiland-logo.webp"
 					width={124}
@@ -31,12 +34,21 @@ function NavBar() {
 					alt="gweiland company logo"
 				/>
 			</div>
-			<div className="flex space-x-5 items-center pr-10">
-				<div>become a affiliate</div>
-				<Twitter fill="white" className="w-5 h-5" />
-				<Facebook fill="white" className="w-5 h-5" />
-				<Instagram className="w-5 h-5" />
-				<UserRound className="w-5 h-5" />
+			<div className="md:hidden">
+				<Image
+					src="/gweiland-logo.webp"
+					width={90}
+					height={90}
+					className="object-contain"
+					alt="gweiland company logo"
+				/>
+			</div>
+			<div className="flex space-x-5 items-center pr-2 md:pr-10">
+				<div className="hidden md:block">become a affiliate</div>
+				<Twitter fill="white" className="w-5 h-5 hidden md:block" />
+				<Facebook fill="white" className="w-5 h-5 hidden md:block" />
+				<Instagram className="w-5 h-5 hidden md:block" />
+				<UserRound className="w-5 h-5 hidden md:block" />
 				<Search className="w-5 h-5" />
 				<CartIcon />
 			</div>
